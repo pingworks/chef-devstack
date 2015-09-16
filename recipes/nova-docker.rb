9a684d740c843bf6c7e436bac33c9db9b8ab1a01
Christoph Lukas <lukas@pingworks.de>
2015-09-16 13:14:00 +0200

    cherry-pick mtu and lo interface fixes from master, fix key injection

--- recipes/nova-docker.rb
+++ recipes/nova-docker.rb
@@ -11,13 +11,12 @@ bash 'checkout nova-docker' do
   code <<-EOF
   export HOME=/home/ubuntu
   git clone -b stable/kilo #{node['devstack']['gitmirror']}/stackforge/nova-docker.git
-  # Cherry pick the mtu fix from master
   EOF
   not_if 'test -d /home/ubuntu/nova-docker/.git'
 end
 
-cookbook_file 'nova-docker-fix1.patch' do
-  path '/home/ubuntu/nova-docker/nova-docker-fix1.patch'
+cookbook_file '0001-Fix-key-injection-when-nova-is-running-as-non-root-u.patch' do
+  path '/home/ubuntu/nova-docker/0001-Fix-key-injection-when-nova-is-running-as-non-root-u.patch'
   owner 'root'
   group 'root'
   mode '644'
@@ -31,7 +30,12 @@ bash 'patch nova-docker' do
   export HOME=/home/ubuntu
   git config user.email "christoph.lukas@gmx.net"
   git config user.name "Christoph Lukas"
+  # Cherry pick the 'lo interface up' and mtu fix from master
+  git cherry-pick ba545170399bbb76e8b2c26ecc082be5e867ee8d
+  git cherry-pick 12b11b31b6f4274efa88f3011d2e4b884ed0e30c
   git cherry-pick 3b3a0c744de288ef3477c6b2940e6308357777f2
+  git cherry-pick 005921b0741f6753799807af6b4444153888851a
+  git am < 0001-Fix-key-injection-when-nova-is-running-as-non-root-u.patch
   EOF
   not_if 'cd /home/ubuntu/nova-docker/ && git log -2 --oneline | grep "vif: Setup MTU on if_remote_name when attach"'
 end
