d1045e09ab67dab42459d8cf031c0e5d7849904a
Christoph Lukas <lukas@pingworks.de>
2015-09-16 13:14:49 +0200

    allow all network traffic for instances

--- recipes/secgroup.rb
+++ recipes/secgroup.rb
@@ -3,8 +3,8 @@ bash 'setup secgroup' do
   code <<-EOF
   source /home/ubuntu/devstack/openrc demo
   nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
-  nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
-  nova secgroup-add-rule default tcp 80 80 0.0.0.0/0
+  nova secgroup-add-rule default tcp 1 65535 0.0.0.0/0
+  nova secgroup-add-rule default udp 1 65535 0.0.0.0/0
   EOF
   not_if '/home/ubuntu/openstack.sh demo nova secgroup-list-rules default | grep icmp'
 end
