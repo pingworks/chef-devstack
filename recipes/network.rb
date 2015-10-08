88e89d3dcfcce32cde35b4dd1f5cdacaced73c8c
Christoph Lukas <lukas@pingworks.de>
2015-10-08 14:55:35 +0200

    reworked networking config

--- recipes/network.rb
+++ recipes/network.rb
@@ -4,3 +4,27 @@ template '/etc/network/interfaces' do
   group 'root'
   mode '0755'
 end
+
+if node['devstack']['type'] == 'gateway' then
+  bash 'setup masquerading' do
+    user 'root'
+    code "iptables -t nat -A POSTROUTING -o #{node['devstack']['external_iface']} -j MASQUERADE"
+  end
+
+  bash 'add masquerading to rc.local' do
+    user 'root'
+    code <<-EOH
+    sed -i -e 's;exit 0;;' /etc/rc.local
+    echo 'iptables -t nat -A POSTROUTING -o #{node['devstack']['external_iface']} -j MASQUERADE' >> /etc/rc.local
+    EOH
+    not_if 'grep "MASQUERADE" /etc/rc.local'
+  end
+else
+  bash 'fix routing' do
+    user 'root'
+    code <<-EOH
+    ip route del default
+    ip route add default via #{node['devstack']['global_gw_ip']}
+    EOH
+  end
+end
